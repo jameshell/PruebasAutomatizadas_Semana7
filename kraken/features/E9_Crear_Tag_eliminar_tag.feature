461352186e9522196bf2db9b98a4090131109059
Pablo9211 <josoriob1@gmail.com>
2024-11-13 09:52:23 -0500

    refactor test 6,7,8,9,10

--- kraken/features/E9_Crear_Tag_eliminar_tag.feature
+++ kraken/features/E9_Crear_Tag_eliminar_tag.feature
@@ -5,22 +5,15 @@ Scenario: Crear tag con datos validos y eliminarlo
   Given the user navigates to Ghost at "<GHOST>"
   And the user logs in using the credentials "<USERNAME>" and "<PASSWORD>"
   And the user navigates to the tags page
-  And I wait for 2 seconds
+  And the user cliks on the new tag
+  And enter name tag to delete "ToDelete"
+  And enter description tag "<TAGDESCRIPTION>"
+  And click in save tag
+
+  When click again tags
+  And click in tag to delete
+  And click delete
   
-  When the user cliks on the new tag
-  And I wait for 1 seconds
-  And I enter name tag to delete "ToDelete"
-
-  And I enter description tag "<TAGDESCRIPTION>"
-  Then I click in save tag
-  And I wait for 1 seconds
-
-  Then I click again tags
-  And I wait for 2 seconds
-  And I click in tag to delete
-  And I wait for 2 seconds
-  And I click delete
-  And I wait for 2 seconds
-  Then I click confirm delete
-  And I wait for 2 seconds
+  Then click confirm delete
+  
 
--- kraken/features/web/step_definitions/createTagsSteps.js
+++ kraken/features/web/step_definitions/createTagsSteps.js
@@ -23,49 +23,49 @@ When("the user cliks on the new tag", async function () {
     await TagPage.navigateToNewTags(this);
 })
 
-When("I enter name tag {kraken-string}", async function (name) {
+When("enter name tag {kraken-string}", async function (name) {
     await TagPage.fillNewTags(this, name);
 })
 
-When("I enter description tag {kraken-string}", async function (text) {
+When("enter description tag {kraken-string}", async function (text) {
     await TagPage.fillDescriptionTags(this, text);
 })
 
-When("I click in save tag", async function () {
+When("click in save tag", async function () {
     await TagPage.saveTag(this);
 })
 
 
-When("I click again tags", async function () {
-    await TagPage.clickInTags(this);
-})
-
-When("I enter invalid name tag", async function () {
+When("enter invalid name tag", async function () {
     const randomText = await generateRandomText(30)
     await TagPage.fillNewTags(this, randomText);
 })
 
 
-When("I enter invalid description tag", async function () {
+When("enter invalid description tag", async function () {
     const randomText = await generateRandomText(501)
     await TagPage.fillInvalidDescriptionTags(this, randomText);
 })
 
-When("I enter name tag to delete {string}", async function (name) {
+When("enter name tag to delete {string}", async function (name) {
     await TagPage.fillNewTagsToDelete(this, name);
 })
 
 
-When("I click in tag to delete", async function () {
+When("click in tag to delete", async function () {
     await TagPage.navigateToTagDelete(this);
 })
 
 
-When("I click delete", async function () {
+When("click delete", async function () {
     await TagPage.clickTagDelete(this);
 })
 
 
-Then("I click confirm delete", async function () {
+Then("click confirm delete", async function () {
     await TagPage.confirmTagDelete(this);
+})
+
+Then("click again tags", async function () {
+    await TagPage.clickInTags(this);
 })
