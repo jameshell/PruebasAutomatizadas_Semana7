713c1cc1ac9c7b37f4347b8f123e904cd0a93cb4
jposoriob1 <josoriob1@gmail.com>
2024-11-15 18:08:18 -0500

    Caputars a los test 6,7,8,9,10 funcinal para rc y rb

--- kraken/features/E8_Crear_Tag_con _datos _vacios.feature
+++ kraken/features/E8_Crear_Tag_con _datos _vacios.feature
@@ -1,12 +1,13 @@
 Feature: Crear tag
 
 @user1 @web
-Scenario: Crear tag con datos vacios
+Scenario: E8 - Crear tag con datos vacios
   Given the user navigates to Ghost at "<GHOST>"
   And the user logs in using the credentials "<USERNAME>" and "<PASSWORD>"
+  And I save device snapshot in sequential file 'E8' '1'
   And the user navigates to the tags page
-  
+  And I save device snapshot in sequential file 'E8' '2'
   When the user cliks on the new tag
-  
+  And I save device snapshot in sequential file 'E8' '3'
   Then click in save tag
-  
+  And I save device snapshot in sequential file 'E8' '4'
--- kraken/features/web/step_definitions/sharedSteps.js
+++ kraken/features/web/step_definitions/sharedSteps.js
@@ -1,9 +1,24 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const LoginPage = require('../../pages/loginPage');
 
+const path = require('path');
+const fs = require('fs');
+
 // Given
 Given('the user navigates to Ghost at {kraken-string}', async function (url) {
     await this.driver.url(url);
+});
+
+Given('I save device snapshot in sequential file {string} {string}', async function (DIR, counter) {
+    const folderPath = path.resolve('screenshots/'+String(DIR));
+
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+    }
+    const filename = `${DIR}-${counter}.png`;
+    const filepath = path.join(folderPath, filename);
+    await this.driver.saveScreenshot(filepath);
+    console.log(`Screenshot saved at: ${filepath}`);
 });
 
 Given('the user logs in using the credentials {kraken-string} and {kraken-string}', async function (username, password) {
