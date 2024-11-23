5104aacb05fe9d18b6faa7819de0a5c3874926b7
Jaime Alonso <jaimea111@gmail.com>
2024-11-23 10:16:35 -0500

    Adding E29 test

--- cypress/cypress/e2e/E29-crear_page_completo_sin_titulo_1000_caracteres.cy.js
+++ cypress/cypress/e2e/E29-crear_page_completo_sin_titulo_1000_caracteres.cy.js
@@ -1,11 +1,10 @@
 import GivenSteps from "./steps/givenSteps";
 import pagesPage from "./pages/pagesPage";
-import { faker } from "@faker-js/faker";
 import WhenStepsPages from "./steps/whenStepsPages";
 import ThenStepsPages from "./steps/thenStepsPages";
 
 
-describe("Pages - Create Page With Only Description", () => {
+describe("Pages - Create Page With Only Description with 1000 characters", () => {
 
     beforeEach(() => {
         GivenSteps.givenNavigateToLoginPage();
@@ -19,10 +18,10 @@ describe("Pages - Create Page With Only Description", () => {
         pagesPage.AndScreenshot('E18-596','1');
     });
 
-    it('E27 - Should create a new page successfully without title', () => {
+    it('E29 - Should create a new page successfully without title', () => {
 
         cy.fixture('pages').then((data) => {
-            const randomPageDescription = data[0].Description;
+            const randomPageDescription = data[0].Description_1000;
 
             WhenStepsPages.WhenNewPageBtn_Click();
             pagesPage.AndScreenshot('E18-596','2');
