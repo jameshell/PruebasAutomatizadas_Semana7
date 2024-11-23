ef0d0de18a043b5b2e9383be0de3b1f6991d75a9
Carlos Bayona <c.bayona@uniandes.edu.co>
2024-11-23 15:18:44 -0500

    8

--- cypress/cypress/e2e/8_member_crear_sin_nombre_correo_valido.cy.js
+++ cypress/cypress/e2e/8_member_crear_sin_nombre_correo_valido.cy.js
@@ -1,11 +1,29 @@
 import givenStepsMembers from "./steps/givenStepsMembers";
+const Mockaroo = require('mockaroo');
+const client = new Mockaroo.Client({ apiKey: "API_KEY" });
+
+const posts = await client.generate({
+    count: 10,
+    schema: [
+        { name: 'name', type: 'string' },
+        { name: 'name', type: 'int' },
+        { name: 'name', type: '' }
+    ],
+  });
+
+rand 1 a 10
+
+posts[rand]
+
+
+
 describe("Pages - Create member", () => {
 
     beforeEach(() => {
+
     });
 
     it('8 - Should create a new member wiuthout name and vailid email', () => {
-        givenStepsMembers.givenNavigateToLoginPage();
-        givenStepsMembers.
+        givenStepsMembers.fillform(post[4].name, post[4].email);
     });
 });
