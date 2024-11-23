5e8c8758edae1e191e68d3a34e4fafac1488b0fe
Jaime Alonso <jaimea111@gmail.com>
2024-11-23 11:57:30 -0500

    Adding test E33

--- cypress/cypress/e2e/E33-draft_con_titulo_menor_255_caracteres.cy.js
+++ cypress/cypress/e2e/E33-draft_con_titulo_menor_255_caracteres.cy.js
@@ -1,6 +1,5 @@
 import GivenSteps from "./steps/givenSteps";
 import pagesPage from "./pages/pagesPage";
-import { faker } from "@faker-js/faker";
 import WhenStepsPages from "./steps/whenStepsPages";
 import ThenStepsPages from "./steps/thenStepsPages";
 
@@ -15,31 +14,31 @@ describe("Pages - Create a complete valid Page with apriori data", () => {
             disableTimersAndAnimations: false,
         })
 
-        pagesPage.AndScreenshot('E26-596','1');
+        pagesPage.AndScreenshot('E33-596','1');
     });
 
-    it('E26 - Should create a new page successfully with apriori data', () => {
+    it('E33 - Should create a new page successfully with apriori data', () => {
         cy.fixture('pages').then((data) => {
             const randomPageDescription = data[0].Description;
             const randomPageTitle = data[0].Title;
 
             WhenStepsPages.WhenNewPageBtn_Click();
-            pagesPage.AndScreenshot('E26-596','2');
+            pagesPage.AndScreenshot('E33-596','2');
 
             WhenStepsPages.WhenFillPageHeader(randomPageTitle);
-            pagesPage.AndScreenshot('E26-596','3');
+            pagesPage.AndScreenshot('E33-596','3');
 
             WhenStepsPages.WhenFillPageDescription(randomPageDescription);
-            pagesPage.AndScreenshot('E26-596','4');
+            pagesPage.AndScreenshot('E33-596','4');
 
             WhenStepsPages.WhenClickAddImgBtn();
-            pagesPage.AndScreenshot('E26-596','5');
+            pagesPage.AndScreenshot('E33-596','5');
 
             WhenStepsPages.WhenClickAddImageBtn_FirstPic()
-            pagesPage.AndScreenshot('E26-596','6');
+            pagesPage.AndScreenshot('E33-596','6');
 
             WhenStepsPages.WhenClickPublishButton();
-            pagesPage.AndScreenshot('E26-596','7');
+            pagesPage.AndScreenshot('E33-596','7');
 
             WhenStepsPages.WhenClickFinalReviewButton();
 
@@ -50,7 +49,7 @@ describe("Pages - Create a complete valid Page with apriori data", () => {
             ThenStepsPages.thenIsModalHeaderCorrect(randomPageTitle);
 
             ThenStepsPages.thenIsModalDescriptionCorrect(randomPageDescription);
-            pagesPage.AndScreenshot('E26-596','8');
+            pagesPage.AndScreenshot('E33-596','8');
         });
     });
 });
