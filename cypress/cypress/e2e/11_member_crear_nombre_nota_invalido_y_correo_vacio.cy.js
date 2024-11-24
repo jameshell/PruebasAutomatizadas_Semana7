5b6722c52c7d35a401342f978c61c71f7cb52777
Carlos Bayona <c.bayona@uniandes.edu.co>
2024-11-23 21:00:24 -0500

    Update member Steps

---
+++ cypress/cypress/e2e/11_member_crear_nombre_nota_invalido_y_correo_vacio.cy.js
@@ -0,0 +1,40 @@
+import GivenStepsMembers from "./steps/givenStepsMembers";
+import WhenStepsMembers from "./steps/whenStepsMembers";
+import ThenStepsMembers from "./steps/thenStepsMembers";
+
+describe("Create Member - Invalid Name and Note and Empty Email", () => {
+    
+
+    it('Should Create a Member', () => {
+        cy.fixture('members.json').then(
+            (mockData) => {
+                // Given the user navigates to Login Page
+                GivenStepsMembers.givenNavigateToLoginPage()
+                // And Logs in
+                GivenStepsMembers.givenLogin();
+
+                // And Navigates to the members page
+                GivenStepsMembers.givenNavigateToMembersPage()
+
+                // And Clicks The New Member Button
+                GivenStepsMembers.givenClicksNewMember()
+
+                // And Fills the Form information
+                let name = mockData[0].invalidName
+                let email = ''
+                let note = mockData[0].invalidNote
+                GivenStepsMembers.givenFillMemberForm(name,email,note)
+
+                // When the user clicks on Save
+                WhenStepsMembers.whenClickSaveMember()
+
+                // Then the user should see the "Please enter an email"
+                ThenStepsMembers.thenSeeEmailRequiredMemberForm()
+                // And "Note is too long."
+                ThenStepsMembers.thenSeeInvalidNote()
+                // And "Name cannot be longer than 191 characters."
+                ThenStepsMembers.thenSeeInvalidName()
+            }
+        )
+    });
+});
